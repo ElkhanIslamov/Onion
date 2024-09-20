a3df65ca52d866683aa0127f5505d4b3d7040346
Elxan Islamov <elkhan.islamov@acsc.az>
2024-09-20 16:55:29 +0400

    mapper

--- Business/MappingProfiles/CategoryMapper.cs
+++ Business/MappingProfiles/CategoryMapper.cs
@@ -9,8 +9,7 @@ public class CategoryMapper:Profile
     public CategoryMapper()
     {
         CreateMap<Category, CategoryGetDto>().ReverseMap(); 
-        CreateMap<CategoryPostDto,Category>().ForMember(x=>x.CreatedDate,y=>y.MapFrom(x=>DateTime.UtcNow))
-            .ForMember(x=>x.UpdatedDate,y=>y.MapFrom(x=>DateTime.UtcNow)).ReverseMap();
+        CreateMap<CategoryPostDto,Category>().ReverseMap();
     }
 
 }
--- Business/MappingProfiles/ProductMapper.cs
+++ Business/MappingProfiles/ProductMapper.cs
@@ -9,11 +9,8 @@ public class ProductMapper:Profile
     public ProductMapper()
     {
         CreateMap<Product,ProductGetDto>().ReverseMap();
-        CreateMap<ProductPostDto,Product>().ForMember(x=> x.CreatedDate, y=>y.MapFrom(x=>DateTime.UtcNow))
-            .ForMember(x=>x.UpdatedDate,y=>y.MapFrom(x=>DateTime.UtcNow))
-            .ForMember(x=>x.IsInStock,y=>y.MapFrom(x => true)).ReverseMap();
-        CreateMap<ProductPutDto,Product>().ForMember(x=>x.UpdatedDate,y=>y.MapFrom(x=> DateTime.UtcNow))
-            .ReverseMap();
+        CreateMap<ProductPostDto,Product>().ForMember(x=>x.IsInStock,y=>y.MapFrom(x => true)).ReverseMap();
+        CreateMap<ProductPutDto,Product>().ReverseMap();
 
     }
 }
